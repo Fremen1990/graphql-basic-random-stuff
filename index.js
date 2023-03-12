6eca4d76275bd45cdff8d7c35339cc8b4eabf7c2
Tomasz Stanisz <fremen1990@gmail.com>
2023-03-12 19:54:39 +0100

    Create simple graphQL server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("TEST nodemon works!")
+const {ApolloServer, ServerInfo} = require("apollo-server")
+
+const typeDefs = `
+type Query {
+greeting : String}
+`
+
+const server = new ApolloServer({
+    typeDefs
+})
+server.listen({port: 4000}).then((result) => {
+    console.log(`Server ready at ${result.url}`)
+    // console.log("result.server", result.server)
+    // console.log("result.port", result.port)
+    // console.log("result.family", result.family)
+})
