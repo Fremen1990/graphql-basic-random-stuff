ef0f695b5bd2663130a14de9ed007fcbcf5b1a38
Tomasz Stanisz <fremen1990@gmail.com>
2023-03-13 00:47:06 +0100

    Configure instrospection and playground

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const data = {
 }
 
 const server = new ApolloServer({
-    typeDefs, rootValue: data
+    typeDefs, rootValue: data, playground: true, introspection: true
 })
-server.listen({port:PORT}).then((result) => {
+server.listen({port: PORT}).then((result) => {
     console.log(`Server ready at ${result.url}`)
     // console.log("result.server", result.server)
     // console.log("result.port", result.port)
