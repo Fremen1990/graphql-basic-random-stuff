969a5f49d19e8262c43f5f50e0ce1832c9446edb
Tomasz Stanisz <fremen1990@gmail.com>
2023-03-13 10:25:25 +0100

    Add executable function into root value

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const {ApolloServer, gql} = require("apollo-server")
 
 const PORT = process.env.PORT || 4000
 
-const typeDefs = gql`  type Query {  greeting : String, interestingUrls: [String]}  `
+const typeDefs = gql`  type Query {  greeting : String, interestingUrls: [String], randomDiceThrow: [Int]}  `
 
-const data = {
-    greeting: "Hello World!!!!!!!",
-    interestingUrls: ["https://www.google.com", "https://www.youtube.com"]
-
+function rootValue() {
+    const getRandomDiceThrow = (sides) => {
+        return Math.floor(Math.random() * sides) + 1
+    }
+    return {
+        greeting: "Hello World!!!!!!!",
+        interestingUrls: ["https://www.google.com", "https://www.youtube.com"],
+        randomDiceThrow: [getRandomDiceThrow(6), getRandomDiceThrow(6), getRandomDiceThrow(6)]
+    }
 }
+// TODO Lekcja 18. Float i Boolean
 
 const server = new ApolloServer({
-    typeDefs, rootValue: data, playground: true, introspection: true
+    typeDefs, rootValue: rootValue, playground: true, introspection: true
 })
 server.listen({port: PORT}).then((result) => {
     console.log(`Server ready at ${result.url}`)
