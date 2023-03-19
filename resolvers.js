420cfabccd5bc78e66ac57175351396abe390115
Tomasz Stanisz <fremen1990@gmail.com>
2023-03-19 23:25:06 +0100

    :white_check_mark: WEEK 3
    Lekcja 14. Aliasy w GraphQL

--- resolvers.js
+++ resolvers.js
@@ -1,38 +1,77 @@
-const toDbId = (externalId) => Buffer.from(externalId, "base64").toString();
-const toExternalId = (dbId) => Buffer.from(dbId).toString("base64");
+const decodeBase64 = (base64String) =>
+  Buffer.from(base64String, "base64").toString();
+const encodeBase64 = (rawString) => Buffer.from(rawString).toString("base64");
+
+const toExternalId = (dbId, type) => encodeBase64(`${type}-${dbId}`);
+const toTypeAndDbId = (externalId) => decodeBase64(externalId).split("-", 2);
+const toDbId = (externalId) => toTypeAndDbId(externalId)[1];
+
+const getAnythingByExternalId = (externalId, db) => {
+  const [type, dbId] = toTypeAndDbId(externalId);
+
+  switch (type) {
+    case "Book": {
+      return db.getBookById(dbId);
+    }
+    case "Author": {
+      return db.getAuthorById(dbId);
+    }
+    case "User": {
+      return db.getUserById(dbId);
+    }
+    default:
+      return null;
+  }
+};
 const resolvers = {
   Query: {
     books: (rootValue, { searchQuery }, { db, search }) =>
       searchQuery.length > 0 ? search.findBooks(searchQuery) : db.getAllBooks(),
-    authors: (rootValue, { searchQuery }, { db, search }) =>
-      searchQuery.length > 0
-        ? search.findAuthors(searchQuery)
-        : db.getAllAuthors(),
-    users: (rootValue, { searchQuery }, { db, search }) =>
-      searchQuery.length > 0 ? search.findUsers(searchQuery) : db.getAllUsers(),
-
+    authors: (rootValue, args, { db }) => db.getAllAuthors(),
+    users: (rootValue, args, { db }) => db.getAllUsers(),
     book: (rootValue, { id }, { db }) => db.getBookById(toDbId(id)),
-    author: (rootValue, { id }, { db }) => db.getAuthorById(id),
-    user: (rootValue, { id }, { db }) => db.getUserById(id),
+    author: (rootValue, { id }, { db }) => db.getAuthorById(toDbId(id)),
+    user: (rootValue, { id }, { db }) => db.getUserById(toDbId(id)),
+    anything: (rootValue, { id }, { db }) => getAnythingByExternalId(id, db),
   },
   Book: {
-    id: (book) => toExternalId(book.id),
+    id: (book) => toExternalId(book.id, "Book"),
     author: (book, args, { db }) => db.getAuthorById(book.authorId),
     cover: (book) => ({
       path: book.coverPath,
     }),
   },
   Author: {
-    id: (author) => toExternalId(author.id),
-    books: (author, args, { db }) =>
-      author.bookIds.map((bookId) => db.getBookById(bookId)),
-    photo: (author) => ({ path: author.photoPath }),
+    id: (author) => toExternalId(author.id, "Author"),
+    books: (author, args, { db }) => author.bookIds.map(db.getBookById),
+    photo: (author) => ({
+      path: author.photoPath,
+    }),
+  },
+  Avatar: {
+    image: (avatar) => ({
+      path: avatar.imagePath,
+    }),
   },
   Image: {
-    url: (image, args, context) => context.assetsBaseUrl + image.path,
+    url: (image, args, { baseAssetsUrl }) => baseAssetsUrl + image.path,
   },
   User: {
-    id: (user) => toExternalId(user.id),
+    id: (user) => toExternalId(user.id, "User"),
+  },
+  Anything: {
+    __resolveType: (anything) => {
+      if (anything.title) {
+        return "Book";
+      }
+      if (anything.bio) {
+        return "Author";
+      }
+      if (anything.info) {
+        return "User";
+      }
+      return null;
+    },
   },
 };
 
--- typeDefs.js
+++ typeDefs.js
@@ -3,7 +3,6 @@ const typeDefs = gql`
   schema {
     query: Query
   }
-
   """
   ### The object representing a books
 
@@ -12,30 +11,29 @@ const typeDefs = gql`
   - author: String
   """
   type Query {
-    authors(searchQuery: String! = ""): [Author!]!
+    authors: [Author!]!
     author(id: ID!): Author
     books(searchQuery: String! = ""): [Book!]!
     book(id: ID!): Book
-    users(searchQuery: String! = ""): [User!]!
+    users: [User!]!
     user(id: ID!): User
+    anything(id: ID!): Anything
   }
-
-  type Book {
-    id: ID!
-    title: String!
-    cover: Image!
-    description: String!
-    author: Author
-  }
-
+  union Anything = Book | Author | User
   type Author {
     id: ID!
     name: String!
     photo: Image!
     bio: String!
-    books: [Book]
+    books: [Book!]!
   }
-
+  type Book {
+    id: ID!
+    title: String!
+    cover: Image!
+    author: Author!
+    description: String!
+  }
   type User {
     id: ID!
     name: String!
@@ -43,14 +41,12 @@ const typeDefs = gql`
     info: String!
     avatar: Avatar!
   }
-
   type Image {
     url: String!
   }
-
   type Avatar {
     image: Image!
-    color: String
+    color: String!
   }
 `;
 
